fb3c1a1c86671de90f1eb0f3d361dd31db09e5a5
Leo <108024684+Leoy456@users.noreply.github.com>
2023-07-19 20:31:56 -0400

    more changes

--- NN/test.py
+++ NN/test.py
@@ -1,35 +1,65 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.models import Model
+import numpy as np
+from tensorflow.python.keras.layers import Input, Dense
+from tensorflow.python.keras.models import Model
 
-# Define the number of nodes in the input layer (assuming you flatten the matrix)
-num_input_nodes = 196  # For a 14x14 matrix         
-
-# Create a function to build the neural network
-def build_neural_network():
-    print("we building")
+# Function to build the neural network
+def build_neural_network(num_input_nodes, num_output_nodes):
     # Input layer
     input_layer = Input(shape=(num_input_nodes,), name='input_layer')
-    print("we inputted")
+
     # Hidden layers (you can adjust the number of layers and nodes as needed)
     hidden_layer_1 = Dense(256, activation='relu', name='hidden_layer_1')(input_layer)
-    print("through layer 1")
     hidden_layer_2 = Dense(128, activation='relu', name='hidden_layer_2')(hidden_layer_1)
-    print("through layer 2")
-    # Output layers for node connections and original matrix
-    output_connections = Dense(num_input_nodes, activation='sigmoid', name='output_connections')(hidden_layer_2)
-    output_original_matrix = Dense(num_input_nodes, activation='sigmoid', name='output_original_matrix')(hidden_layer_2)
-    print("output whatever thing")
+
+    # Output layer for node connections
+    output_layer = Dense(num_output_nodes, activation='sigmoid', name='output_layer')(hidden_layer_2)
+
     # Create the model
-    model = Model(inputs=input_layer, outputs=[output_connections, output_original_matrix], name='bridge_design_model')
-    print("model created")
+    model = Model(inputs=input_layer, outputs=output_layer, name='bridge_design_model')
+
     # Compile the model
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-    print("model compiled")
+
     return model
 
-# Build the neural network
-model = build_neural_network()
+# Function to convert output to desired format
+def process_output(output, num_rows, num_columns):
+    node_connections = []
+    for row in range(num_rows):
+        connections = []
+        for col in range(num_columns):
+            node_name = f'n{row}{col}'
+            connected_nodes = [f'n{i}{j}' for i in range(num_rows) for j in range(num_columns) if output[row*num_columns+col][i*num_columns+j] > 0.5]
+            connections.append([node_name] + connected_nodes)
+        node_connections.extend(connections)
+    return node_connections
 
-# Display the model summary
-model.summary()
+# Function for training the model
+def train_model(model, input_data, target_data, cost):
+    model.fit(input_data, target_data, epochs=100, batch_size=32, verbose=1)
+
+# Example usage
+if __name__ == '__main__':
+    # Example input matrix (unflattened)
+    input_matrix = np.random.randint(0, 2, (5, 5))  # Replace this with your actual input matrix
+
+    num_rows, num_columns = input_matrix.shape
+
+    # Flatten the input matrix
+    flattened_input = input_matrix.flatten()
+
+    # Build the model
+    model = build_neural_network(num_rows * num_columns, num_rows * num_columns)
+
+    # Train the model (assuming you have the target data and cost function)
+    # Replace target_data and cost with your actual data and cost function
+    target_data = np.random.randint(0, 2, (num_rows * num_columns, num_rows * num_columns))
+    train_model(model, flattened_input.reshape(1, -1), target_data, cost=0.5)
+
+    # Get the model's output
+    output = model.predict(flattened_input.reshape(1, -1))
+
+    # Process the output into the desired format
+    node_connections = process_output(output, num_rows, num_columns)
+    print(node_connections)
